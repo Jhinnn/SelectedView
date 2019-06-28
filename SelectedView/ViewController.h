c5a3babade56eb75473af6c41202a73db5316aad
pengpengduduguagua <2979468570@qq.com>
2019-06-28 17:25:48 +0800

    Initial Commit

---
+++ SelectedView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectedView
+//
+//  Created by admin on 2019/6/28.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SelectedView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SelectedView
+//
+//  Created by admin on 2019/6/28.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
