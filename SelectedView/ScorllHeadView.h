ac7dc2bafbbc6268b3923e01979bde84ddab3da0
pengpengduduguagua <2979468570@qq.com>
2019-06-28 18:41:14 +0800

    first commit

---
+++ SelectedView/ScorllHeadView.h
@@ -0,0 +1,17 @@
+//
+//  ScorllHeadView.h
+//  SelectedView
+//
+//  Created by admin on 2019/6/28.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScorllHeadView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SelectedView/ScorllHeadView.m
@@ -0,0 +1,21 @@
+//
+//  ScorllHeadView.m
+//  SelectedView
+//
+//  Created by admin on 2019/6/28.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import "ScorllHeadView.h"
+
+@implementation ScorllHeadView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
